1e9a6a0317891e2ec63fb9df99340f78fcadc846
Dima <Deminguru01@gmail.com>
2022-12-14 10:25:34 +0500

    Add project files.

---
+++ MauiApp3/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiApp3;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+
+        BindingContext = new MainViewModel
+        {
+            Title = "MyTitle"
+        };
+    }
+}
+
---
+++ MauiApp3/MainViewModel.cs
@@ -0,0 +1,61 @@
+﻿
+
+using System;
+
+namespace MauiApp3;
+
+
+public class MainViewModel : BindableObject
+{
+    private DateTime _dateTime;
+    private Timer _timer;
+    //public MainViewModel()
+    //{
+    //    var timer = new Timer(state =>
+    //   {
+    //        OnPropertyChanged(nameof(time));
+    //    }, new object(), TimeSpan.Zero, TimeSpan.FromSeconds(1));
+    //}
+    private string _title;
+
+    public string Title
+    {
+        get => _title;
+        set
+        {
+            if (_title == value) return;
+            _title = value;
+            OnPropertyChanged(nameof(Title));
+        }
+    }
+
+   
+    public MainViewModel()
+    {
+
+        _timer = new Timer(new TimerCallback((s) =>
+            {
+                //this.DateTime = DateTime.Now;
+                OnPropertyChanged(nameof(time));
+                OnPropertyChanged(nameof(test));
+            }),
+            null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+    }
+
+
+    public int[] time
+    {
+        get
+        {
+            var dateTime = DateTime.Now;
+            return new int[6]
+            {
+                dateTime.Hour / 10, dateTime.Hour % 10,
+                dateTime.Minute / 10, dateTime.Minute % 10,
+                dateTime.Second / 10, dateTime.Second %10
+            };
+        }
+    }
+
+    public String test => new string(time.Select(x => x.ToString().ToCharArray()[0]).ToArray());
+}
